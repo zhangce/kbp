98b2305d047c4fcc47aa150dd03231997e7e8d30
Mikhail Sushkov <sushkovm@gmail.com>
2014-05-20 16:27:13 -0700

    dependency path using string library

--- udf/dummy_json.py
+++ udf/dummy_json.py
@@ -2,7 +2,7 @@
 
 import sys, json
 
-f = open('/lfs/local/0/msushkov/deepdive/app/kbp/udf/string_library_test.json', 'a')
+f = open('/lfs/local/0/msushkov/deepdive/app/kbp/udf/string_library_test_large.json', 'a')
 
 for row in sys.stdin:
 	f.write(row)
--- udf/ext_relation_mention_feature_mike_json.py
+++ udf/ext_relation_mention_feature_mike_json.py
@@ -9,6 +9,10 @@ from lib import dd
 
 SD = {}
 
+def dep_format_parser(s):
+	parent, label, child = s.split('\t')
+	return (int(parent) - 1, label, int(child) - 1)
+
 for row in sys.stdin:
 	obj = json.loads(row)
 
@@ -16,39 +20,32 @@ for row in sys.stdin:
 	sentence_id = obj['sentence_id']
 	dep_graph = obj['dep_graph']
 	lemma = obj['lemma']
+	words = obj['words']
 
 	mention_ids = obj['mention_ids']
-	words = obj['words']
+	mention_words = obj['mention_words']
 	types = obj['types']
 	starts = obj['starts']
 	ends = obj['ends']
 
-	mentions = zip(mention_ids, words, types, starts, ends)
+	mentions = zip(mention_ids, mention_words, types, starts, ends)
 	mentions = map(lambda x: {"mention_id" : x[0], "word" : x[1], "type" : x[2], "start" : x[3], "end" : x[4]}, mentions)
 
-
-	# at this point we have a list of the mentions in this sentence
 
 	if len(mentions) > 20:
 		break
 
-	if len(words) > 100:
+	if len(lemma) > 100:
 		break
 
-	deptree = {}
-	r = {}
-	
-	try:
-		for edge in dep_graph:
-			edge = re.sub('\s+', ' ', edge)
-			(parent, label, child) = edge.split(' ') 
-			deptree[int(child)-1] = {"label":label, "parent":int(parent)-1}
-			r[int(parent)-1] = 1
-	except:
-		WHY55555555 = True
 
-	if len(r) == 1:
-		deptree = {}
+
+	# list of Word objects
+	words = dd.unpack_words(obj, lemma='lemma', words='words', dep_graph='dep_graph', \
+		dep_graph_parser=dep_format_parser)
+
+
+	# at this point we have a list of the mentions in this sentence
 
 
 	for m1 in mentions:
@@ -67,95 +64,98 @@ for row in sys.stdin:
 
 			features = []
 
+			#
+			# word sequence
+			#
+
+			# the spans of the mentions
 			span1 = dd.Span(begin_word_id=start1, length=end1 - start1)
 			span2 = dd.Span(begin_word_id=start2, length=end2 - start2)
 
+			# the lemma sequence between the mention spans
 			lemma_between = dd.tokens_between_spans(lemma, span1, span2)
 			if lemma_between.is_inversed:
 				feature = "WORDSEQ_INV:" + "_".join(lemma_between.elements).lower()
 			else:
-				feature = "WORDSEQ_" + "_".join(lemma_between.elements).lower()
+			    feature = "WORDSEQ_" + "_".join(lemma_between.elements).lower()
+
 			features.append(feature)
 
 
+			#
+			# dependency path
+			#
 
-			if len(deptree) > 0:
-				path1 = []
-				end = end1 - 1
-				ct = 0
-				while True:
-					ct = ct + 1
-					if ct > 100:
-						break
-					if end not in deptree:
-						path1.append({"current":end, "parent": -1, "label":"ROOT"})
-						break
-					path1.append({"current":end, "parent": deptree[end]["parent"], "label":deptree[end]["label"]})
-					end = deptree[end]["parent"]
+			# list of DepEdge objects representing the dependency path
+			edges = dd.dep_path_between_words(words, end1 - 1, end2 - 1)
 
-				path2 = []
-				end = end2 - 1
-				ct = 0
-				while True:
-					ct = ct + 1
-					if ct > 100:
-						break
-					if end not in deptree:
-						path2.append({"current":end, "parent": -1, "label":"ROOT"})
-						break
-					path2.append({"current":end, "parent": deptree[end]["parent"], "label":deptree[end]["label"]})
-					end = deptree[end]["parent"]
+			if len(edges) > 0:
+				# find the index of the edge which is pointing in a different direction than
+				# the one before it
+				switch_direction_index = -1
 
-				commonroot = None
-				for i in range(0, len(path1)):
-					j = len(path1) - 1 - i
-					if -i-1 <= -len(path2) or path1[j]["current"] != path2[-i-1]["current"]:
+				for i in range(len(edges)):
+					edge = edges[i]
+					if not edge.is_bottom_up:
+						switch_direction_index = i
 						break
-					commonroot = path1[j]["current"]
+
+				# Word = collections.namedtuple('Word', ['begin_char_offset', 'end_char_offset', 'word', 'lemma', 'pos', 'ner', 'dep_par', 'dep_label'])
 
 				left_path = ""
-				lct = 0
-				for i in range(0, len(path1)):
-					lct = lct + 1
-					if path1[i]["current"] == commonroot:
-						break
-					if path1[i]["parent"] == commonroot or path1[i]["parent"]==-1:
-						left_path = left_path + ("--" + path1[i]["label"] + "->" + '|')
-					else:
-						w = lemma[path1[i]["parent"]].lower()
-						if i == 0: 
-							w = ""
-						left_path = left_path + ("--" + path1[i]["label"] + "->" + w)
-				
 				right_path = ""
-				rct = 0
-				for i in range(0, len(path2)):
-					rct = rct + 1
-					if path2[i]["current"] == commonroot:
-						break
-					if path2[i]["parent"] == commonroot or path2[i]["parent"]==-1:
-						right_path = ('|' + "<-" + path2[i]["label"] + "--") + right_path
-					else:
-						w = lemma[path2[i]["parent"]].lower()
-						if i == 0:
-							w = ""
-						right_path = (w + "<-" + path2[i]["label"] + "--" ) + right_path
+				num_left = 0
+				num_right = 0
 
-				path = ""
-				if commonroot == end1-1 or commonroot == end2-1:
-					path = left_path + "SAMEPATH" + right_path
-				else:
-					if commonroot != None:
-						path = left_path + lemma[commonroot].lower() + right_path
+				# iterate through the edge list
+				for i in range(len(edges)):
+					# DepEdge = collections.namedtuple('DepEdge', ['word1', 'word2', 'label', 'is_bottom_up'])
+					curr_edge = edges[i]
+
+					# going from the left to the root
+					if curr_edge.is_bottom_up:
+						num_left = num_left + 1
+
+						# if this is the edge pointing to the root (word2 is the root)
+						if i == switch_direction_index - 1:
+							left_path = left_path + ("--" + curr_edge.label + "->|")
+							root = curr_edge.word2.lemma.lower()
+
+						# this edge does not point to the root
+						else:
+							left_path = left_path + ("--" + curr_edge.label + "->" + curr_edge.word2.lemma.lower())
+					
+					# going from the root to the right
 					else:
-						path = left_path + "NONEROOT" + right_path
-				if path != "":
-					features.append(path)
+						num_right = num_right + 1
+
+						# if this is the edge pointing from the root (word1 is the root)
+						if i == switch_direction_index - 1:
+							right_path = ("|<-" + curr_edge.label + "--") + right_path
+							assert root == curr_edge.word1.lemma.lower()
+
+						# this edge does not point from the root
+						else:
+							right_path = (curr_edge.word1.lemma.lower() + "<-" + curr_edge.label + "--") + right_path
+
+				# if the edge arrows never switched direction, we don't have a root
+				if switch_direction_index == -1:
+					root = "NONEROOT"
+
+				# if the root is at the end of at the beginning
+				elif switch_direction_index == len(edges) - 1 or switch_direction_index == 0:
+					root = "SAMEPATH"
+
+				path = left_path + root + right_path
+				features.append(path)
 
 				if 'wife' in path or 'widow' in path or 'husband' in path:
-					features.append('LEN_%d_wife/widow/husband' % (lct + rct))
+					features.append('LEN_%d_wife/widow/husband' % (num_left + num_right))
 
+			
 			print json.dumps( {"doc_id":doc_id, "mid1": m1["mention_id"], "mid2": m2["mention_id"], "word1": m1["word"], "word2": m2["word"], "features":features, "type1":m1["type"], "type2":m2["type"]} )
+
+
 
 
 
