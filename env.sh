d05f8cb839d46c92812ec6bfdf8d79f7e9d592d7
zhangce <czhang@cs.wisc.edu>
2014-05-22 04:32:49 -0700

    able to run

--- env.sh
+++ env.sh
@@ -12,10 +12,10 @@ export SBT_OPTS="-Xmx$MEMORY"
 
 # Database Configuration (default)
 #export DBNAME="deepdive_deepdive_mikhail"
-export PGUSER=${PGUSER:-`whoami`}
-export PGPASSWORD=${PGPASSWORD:-}
-export PGPORT=${PGPORT:-5432}
-export PGHOST=${PGHOST:-localhost}
+#export PGUSER=${PGUSER:-`whoami`}
+#export PGPASSWORD=${PGPASSWORD:-}
+#export PGPORT=${PGPORT:-5432}
+#export PGHOST=${PGHOST:-localhost}
 
 #export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/dfs/rulk/0/hazy_share/lib64/:/dfs/rulk/0/hazy_share/lib/protobuf/lib/:/dfs/rulk/0/hazy_share/lib/tclap/lib/"
 
--- env_db.sh
+++ env_db.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 export DBNAME=deepdive_kbp_mikhail
+export PGUSER=`whoami`
 export PGHOST=localhost
 export PGPORT=5432
