351c965a656226bc81bace631915291c4aa1fa52
Mikhail Sushkov <sushkovm@gmail.com>
2014-05-23 10:50:36 -0700

    updating initial database

--- env.sh
+++ env.sh
@@ -17,12 +17,10 @@ export PGPASSWORD=${PGPASSWORD:-}
 export PGPORT=${PGPORT:-5432}
 export PGHOST=${PGHOST:-localhost}
 
-#export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/dfs/rulk/0/hazy_share/lib64/:/dfs/rulk/0/hazy_share/lib/protobuf/lib/:/dfs/rulk/0/hazy_share/lib/tclap/lib/"
-
 # the deepdive_kbp DB dump files (2 of them to fit Github's size limit)
 export DB_DUMP_FILE_1=$APP_HOME/data/deepdive_kbp.sql.tar.bz2.1
 export DB_DUMP_FILE_2=$APP_HOME/data/deepdive_kbp.sql.tar.bz2.2
 
 # the combined DB dump file that will be used to recreate the DB
 export DB_DUMP_FILE_COMBINED=$APP_HOME/data/deepdive_kbp.sql.tar.bz2
-export DB_DUMP_FILE_UNCOMPRESSED=$APP_HOME/deepdive_kbp.sql
+export DB_DUMP_FILE_UNCOMPRESSED=$APP_HOME/data/deepdive_kbp.sql
